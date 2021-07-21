8ed2233218e7ae8c116abc43661819d74c5708ff
Dilshod Turobov <deebov@yandex.com>
2021-07-21 23:10:24 +0200

    feat(Select): export core primitive components

---
+++ packages/select/index.ts
@@ -0,0 +1,24 @@
+export { Root } from "./core/Root";
+export type { RootProps } from "./core/Root";
+
+export { Option } from "./core/Option";
+export type { OptionProps } from "./core/Option";
+
+export { Listbox } from "./core/Listbox";
+export type { ListboxProps } from "./core/Listbox";
+
+export { Section } from "./core/Section";
+export type { SectionProps } from "./core/Section";
+
+export { Trigger } from "./core/Trigger";
+export type { TriggerProps } from "./core/Trigger";
+
+export { Label } from "./core/Label";
+export type { LabelProps } from "./core/Label";
+
+export { Divider } from "./core/Divider";
+export type { DividerProps } from "./core/Divider";
+
+export { useSelectState } from "./hooks/useSelectState";
+export type { UseSelectStateOptions, UseSelectStateReturn, Value } from "./hooks/useSelectState";
+
