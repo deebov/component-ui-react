fceac2ce7ab5b9bb929f8a55769fe3fef48a1d51
Dilshod Turobov <deebov@yandex.com>
2021-07-21 23:05:13 +0200

    feat: add useMergedRef

---
+++ packages/select/hooks/useMergedRef.ts
@@ -0,0 +1,5 @@
+export const useMergedRef = <T extends any>(...refs: React.Ref<T>[]): React.RefCallback<T> => (element: T) =>
+    refs.forEach((ref) => {
+        if (typeof ref === "function") ref(element);
+        else if (ref && typeof ref === "object") (ref as React.MutableRefObject<T>).current = element;
+    });
