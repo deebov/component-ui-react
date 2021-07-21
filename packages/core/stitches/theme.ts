4a698f13175b05de659d2111ddc70dc38b8a705e
Dilshod Turobov <deebov@yandex.com>
2021-07-21 02:09:24 +0200

    feat: add stitches theme

---
+++ packages/core/stitches/theme.ts
@@ -0,0 +1,45 @@
+export const mainTheme = {
+    colors: {
+        mainBgColor: "#fff",
+        primary300: "#00A3BE",
+        primary400: "#05778f",
+
+        gray100: "#f9fafc",
+        gray200: "#eceef4",
+        gray300: "#cdd0e0",
+        gray400: "#6e7492",
+        gray500: "#191d2f",
+
+        lightText: "$gray100",
+        darkText: "$gray500",
+        secondaryDarkText: "$gray400",
+    },
+    space: {
+        5: "4px",
+        10: "8px",
+        15: "12px",
+        20: "16px",
+        25: "20px",
+        30: "24px",
+        40: "32px",
+        50: "40px",
+    },
+    sizes: {},
+    fontWeights: {
+        regular: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+    },
+    fontSizes: {
+        1: "12px",
+        2: "16px",
+    },
+    fonts: {
+        main: `-apple-system, "Segoe UI", Roboto, Arial, sans-serif`,
+    },
+    shadows: {
+        md: "0 2px 8px 0 #6e749226",
+    },
+    radii: {},
+};
